0242f3a885e4246ec470c33ff4df6c41d0d0f6bb
안동환 <ddh1713flrwls1713@gmail.com>
2024-08-28 19:56:23 +0900

    assignStart

---
+++ src/main/java/com/sparta/springlv2assign/SpringLv2AssignApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv2assign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLv2AssignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLv2AssignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springlv2assign/SpringLv2AssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv2assign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv2AssignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
